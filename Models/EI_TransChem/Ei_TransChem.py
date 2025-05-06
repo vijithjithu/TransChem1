7cc7878c92e3babdf328c439f104648916a2613a
vijithp <114420915+vijithjithu@users.noreply.github.com>
2025-05-06 11:25:33 +0000

    Your meaningful commit message

--- Models/EI_TransChem/Ei_TransChem.py
+++ Models/EI_TransChem/Ei_TransChem.py
@@ -244,7 +244,7 @@ def test(model, loss_fn, train_dataloader, test_dataloader, device, scaler_targe
 
 def main(finetune_config):
 
-    f = open('Ei.json')
+    f = open('Ei_chem_feature_names.json')
     feats_to_norm = json.load(f)
     best_r2 = 0.0           # monitor the best r^2 in the run
     seed= finetune_config['fold_seed']
@@ -475,7 +475,7 @@ def main(finetune_config):
 
 if __name__ == "__main__":
 
-    finetune_config = yaml.load(open("config_ei_cheminfo.yaml", "r"), Loader=yaml.FullLoader)
+    finetune_config = yaml.load(open("config_Ei_TransChem_finetune_params.yaml", "r"), Loader=yaml.FullLoader)
     print(finetune_config)
 
     """Device"""
